989e94c39c36ae858d21a2817dd2038c66e02827
bcoop <bcoop@live.unc.edu>
2022-04-30 17:12:27 -0400

    testing shifting focus

--- public/main.js
+++ public/main.js
@@ -1,5 +1,17 @@
 // Focus div based on nav button click
+function focusOnHome() {
+    document.getElementById("home").setAttribute("class", "active");
+    document.getElementById("single").setAttribute("class","hidden");
+    document.getElementById("multi").setAttribute("class","hidden");
+    document.getElementById("guess").setAttribute("class","hidden");
+}
 
+function focusOnSingle() {
+    document.getElementById("home").setAttribute("class", "hidden");
+    document.getElementById("single").setAttribute("class","active");
+    document.getElementById("multi").setAttribute("class","hidden");
+    document.getElementById("guess").setAttribute("class","hidden");
+}
 // Flip one coin and show coin image to match result when button clicked
 
 // Flip multiple coins and show coin images in table as well as summary results
